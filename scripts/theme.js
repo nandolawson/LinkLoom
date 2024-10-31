39198160b8888f4a92ab359d2340b170fc86ad35
Nando Lawson <mail@nandolawson.online>
2024-10-31 23:25:47 +0100

    asdf

--- scripts/theme.js
+++ scripts/theme.js
@@ -8,7 +8,7 @@ document.addEventListener("DOMContentLoaded", () => {
             document.documentElement.style.setProperty('--background', theme.background || '#000000');
             document.documentElement.style.setProperty('--button', theme.button || '#333333');
             if (theme.wallpaper = "particledrift") {
-                fetch('.config/wallpaper/particledrift.json')
+                fetch('config/wallpaper/particledrift.json')
                     .then(response => response.json())
                     .then(wallpaper => {particledrift(wallpaper);});
             }

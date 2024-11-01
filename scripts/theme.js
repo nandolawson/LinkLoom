d455467301148830169e583520e40c3a884a576b
Nando Lawson <mail@nandolawson.online>
2024-11-01 13:11:04 +0100

    asdf

--- scripts/theme.js
+++ scripts/theme.js
@@ -1,16 +1,19 @@
-import { particledrift } from scripts/wallpaper/particledrift.js;
 document.addEventListener("DOMContentLoaded", () => {
-    // Fetch all data from theme.json
     fetch('config/theme.json')
         .then(response => response.json())
         .then(theme => {
             document.documentElement.style.setProperty('--accent', theme.accent || '#ffffff');
             document.documentElement.style.setProperty('--background', theme.background || '#000000');
             document.documentElement.style.setProperty('--button', theme.button || '#333333');
-            if (theme.wallpaper = "particledrift") {
-                fetch('config/wallpaper/particledrift.json')
-                    .then(response => response.json())
-                    .then(wallpaper => {particledrift(wallpaper);});
+            if (theme.wallpaper === "particledrift") {
+                import('/scripts/wallpaper/particledrift.js')
+                    .then(module => {
+                        fetch('config/wallpaper/particledrift.json')
+                            .then(response => response.json())
+                            .then(wallpaper => {
+                                module.particledrift(wallpaper);
+                            });
+                    });
             }
         });
 });

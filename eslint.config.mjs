4bdbf2de883531059e6788f7662d5bd46fda4b40
Nando Lawson <mail@nandolawson.online>
2024-11-09 01:28:52 +0100

    Solve most of the complaints ESLint has & restore default configuration

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -8,6 +8,7 @@ export default [
   {languageOptions: { globals: globals.browser }},
   pluginJs.configs.all,
   {rules: {
+    "max-statements": "off",
     "no-magic-numbers": "off",
     "no-plusplus": "off",
     "no-ternary": "off",
--- scripts/theme.js
+++ scripts/theme.js
@@ -6,10 +6,91 @@ Licensed under the GPL, Version 3 <https://github.com/nandolawson/LinkLoom/blob/
 This file may not be copied, modified, or distributed except according to those terms.
 */
 
-
 /* Wallpapers */
 
 const wallpapers = {
+    brokenglass: (wallpaperconfig) => {
+        const wallpaper = document.createElement("div");
+        for (let i = 0; i < wallpaperconfig.amount; i++) {
+            const svg = document.createElementNS("http://www.w3.org/2000/svg", "svg"),
+                polygon = document.createElementNS(svg.namespaceURI,'polygon'),
+                wrapper = document.createElement("div");
+            Object.assign(svg.setAttribute, {
+                height: `${wallpaperconfig.size}vh`,
+                viewBox: "0 0 100 100",
+                width: `${wallpaperconfig.size}vw`
+            });
+
+            Object.assign(wrapper.style, {
+                left: `${Math.random() * 100}%`,
+                position: "absolute",
+                rotate: `${Math.random() * 360}deg`,
+                top: `${Math.random() * 100}%`,
+                transform: "translate(-50%, -50%)",
+            });
+
+            polygon.style.fill = wallpaperconfig.possible_colors[parseInt(Math.random() * wallpaperconfig.possible_colors.length, 10)];
+
+            // Generate random polygon points - min of 3, max of 5 
+            let points = "0,0"
+            for (let i = 0; i < 2 + Math.random() * 2; i++) { 
+                points += ` ${Math.random()*100},${Math.random()*100}`
+            }
+            polygon.setAttribute("points", points);
+
+            svg.appendChild(polygon);
+            wrapper.append(svg);
+            wallpaper.appendChild(wrapper);
+        }
+
+        wallpaper.style = "filter: blur(1vmin);"
+        return wallpaper;
+    },
+
+    characterdrift: (wallpaperconfig) => { 
+        // Inject keyframes for animation - same as particledrift
+        const styleSheet = document.createElement("style");
+        styleSheet.id = "characterdrift-move-keyframes";
+        styleSheet.innerHTML = `@keyframes characterdrift-move {100% { transform: translate3d(0, 0, 1px) rotate(360deg); }}`;
+        document.head.appendChild(styleSheet);
+
+        const randomElementFrom = (array) => array[parseInt(Math.random() * array.length, 10)],
+            wallpaper = document.createElement("div");
+            wallpaper.style = "filter: drop-shadow(0 0 1rem black);"
+        
+
+        for (let amount = 0; amount < (wallpaperconfig.amount || 15); amount++) {
+            const character = document.createElement("span"),
+                fontSize = `${Math.random() * wallpaperconfig.size + 1}rem`;
+
+            character.textContent = randomElementFrom(wallpaperconfig.characters);
+
+            // CSS properties
+            Object.assign(character.style, {
+                animation: `characterdrift-move ${Math.random() * 120 + 30}s linear ${-Math.random() * 200}s infinite`,
+                backgroundColor: wallpaperconfig[`character${Math.floor(Math.random() * 3) + 1}`],
+                borderRadius: "50%",
+                color: randomElementFrom(wallpaperconfig.possible_colors),
+                fontSize,
+                left: `${Math.random() * 100}%`,
+                position: "absolute",
+                top: `${Math.random() * 100}%`,
+                transformOrigin: `${Math.floor(Math.random() * 40 - 20)}vw ${Math.floor(Math.random() * 40 - 20)}vh`,
+            });
+
+            // Add the character to the newly created div
+            wallpaper.appendChild(character);
+        }
+        return wallpaper
+    },
+
+    gradient: (wallpaperconfig) => { 
+        const wallpaper = document.createElement("div");
+        const colors = wallpaperconfig.colors.join(", ")
+        wallpaper.style = `background: linear-gradient(${wallpaperconfig.rotation}deg, ${colors})`
+        return wallpaper;
+    },
+
     particledrift: (wallpaperconfig) => {
         // Get particle colors from config or use default values
         [1, 2, 3].forEach(value => {
@@ -25,7 +106,7 @@ const wallpapers = {
         styleSheet.innerHTML = `@keyframes particledrift-move {100% { transform: translate3d(0, 0, 1px) rotate(360deg); }}`;
         document.head.appendChild(styleSheet);
 
-        let wallpaper = document.createElement("div");
+        const wallpaper = document.createElement("div");
 
         for (let amount = 0; amount < (wallpaperconfig.amount || 15); amount++) {
             const particle = document.createElement("span"),
@@ -49,93 +130,7 @@ const wallpapers = {
             wallpaper.appendChild(particle);
         }
         return wallpaper
-    },
-
-    brokenglass: (wallpaperconfig) => {
-        let wallpaper = document.createElement("div");
-        for (let amount = 0; amount < wallpaperconfig.amount; amount++) {
-            let wrapper = document.createElement("div"),
-               svg = document.createElementNS("http://www.w3.org/2000/svg", "svg"),
-               polygon = document.createElementNS(svg.namespaceURI,'polygon');
-            
-            svg.setAttribute("viewBox", `0 0 100 100`);  // needed for proper resizing
-            svg.setAttribute("width", `${wallpaperconfig.size}vw`);
-            svg.setAttribute("height", `${wallpaperconfig.size}vh`);
-    
-            Object.assign(wrapper.style, {
-                position: "absolute",
-                transform: "translate(-50%, -50%)",  // Center origin for top/left
-                top: `${Math.random() * 100}%`,
-                left: `${Math.random() * 100}%`,
-                rotate: `${Math.random() * 360}deg`,
-            });
-    
-            Object.assign(polygon.style, {
-                fill: randomElementFrom(wallpaperconfig.possible_colors),
-            });
-    
-            // Generate random polygon points - min of 3, max of 5 
-            let points = "0,0"
-            for (let i = 0; i < 2 + Math.random() * 2; i++) { 
-                points += ` ${Math.random()*100},${Math.random()*100}`
-            }
-            polygon.setAttribute("points", points);
-    
-            svg.appendChild(polygon);
-            wrapper.append(svg);
-            wallpaper.appendChild(wrapper);
-        }
-
-        wallpaper.style = "filter: blur(1vmin);"
-        return wallpaper;
-    },
-
-    characterdrift: (wallpaperconfig) => { 
-        // Inject keyframes for animation - same as particledrift
-        const styleSheet = document.createElement("style");
-        styleSheet.id = "characterdrift-move-keyframes";
-        styleSheet.innerHTML = `@keyframes characterdrift-move {100% { transform: translate3d(0, 0, 1px) rotate(360deg); }}`;
-        document.head.appendChild(styleSheet);
-
-        let wallpaper = document.createElement("div");
-        wallpaper.style = "filter: drop-shadow(0 0 1rem black);"
-
-        for (let amount = 0; amount < (wallpaperconfig.amount || 15); amount++) {
-            let particle = document.createElement("span"),
-                fontSize = `${Math.random() * wallpaperconfig.size + 1}rem`;
-
-            particle.textContent = randomElementFrom(wallpaperconfig.characters);
-
-            // CSS properties
-            Object.assign(particle.style, {
-                animation: `characterdrift-move ${Math.random() * 120 + 30}s linear ${-Math.random() * 200}s infinite`,
-                backgroundColor: wallpaperconfig[`particle${Math.floor(Math.random() * 3) + 1}`],
-                borderRadius: "50%",
-                fontSize: fontSize,
-                color: randomElementFrom(wallpaperconfig.possible_colors),
-                left: `${Math.random() * 100}%`,
-                position: "absolute",
-                top: `${Math.random() * 100}%`,
-                transformOrigin: `${Math.floor(Math.random() * 40 - 20)}vw ${Math.floor(Math.random() * 40 - 20)}vh`,
-            });
-
-            // Add the particle to the newly created div
-            wallpaper.appendChild(particle);
-        }
-        return wallpaper
-    },
-
-    gradient: (wallpaperconfig) => { 
-        let wallpaper = document.createElement("div");
-        const colors = wallpaperconfig.colors.join(", ")
-        wallpaper.style = `background: linear-gradient(${wallpaperconfig.rotation}deg, ${colors})`
-        return wallpaper;
     }
-}
-
-
-function randomElementFrom(array) {
-    return array[parseInt(Math.random() * (array.length))]
 }
 
 
@@ -166,7 +161,7 @@ fetch("config/theme.json").then(response => response.json()).then(config => {
     if (config.wallpaper) { config.wallpapers = [config.wallpaper]; }
 
     // Load wallpaper if one is set
-    for (let wallpaper of config.wallpapers) {
+    for (const wallpaper of config.wallpapers) {
         fetch(`config/wallpaper/${wallpaper}.json`).then(response => response.json()).then(wallpaperconfig => {
             document.querySelector("#wallpapers").appendChild(wallpapers[wallpaper](wallpaperconfig));
         });

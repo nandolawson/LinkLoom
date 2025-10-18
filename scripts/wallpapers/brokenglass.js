05d624355ed4dffd6af4090533a9e71f125fa285
Nando Lawson <37831410+nandolawson@users.noreply.github.com>
2025-10-18 15:59:15 +0000

    Translate comments & make ESLint happy

--- scripts/wallpapers/brokenglass.js
+++ scripts/wallpapers/brokenglass.js
@@ -1,35 +1,37 @@
 export const brokenglass = (wallpaperconfig) => {
     const wallpaper = document.createElement("div");
-    wallpaper.style.position = "relative"; // damit absolute Kinder richtig positioniert werden
+    // Position absolute children correctly
+    wallpaper.style.position = "relative";
     wallpaper.style.filter = "blur(1vmin)";
 
     const colors = wallpaperconfig.possible_colors && wallpaperconfig.possible_colors.length
         ? wallpaperconfig.possible_colors
-        : ["#ffffff"]; // Fallback, falls keine Farben definiert sind
+        // Fallback if no colors are defined
+        : ["#ffffff"];
 
     for (let amount = 0; amount < wallpaperconfig.amount; amount++) {
         const svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
         const polygon = document.createElementNS(svg.namespaceURI, "polygon");
         const wrapper = document.createElement("div");
 
-        // SVG Attribute korrekt setzen
+        // Set SVG attributes correctly
         svg.setAttribute("height", `${wallpaperconfig.size}vh`);
         svg.setAttribute("width", `${wallpaperconfig.size}vw`);
         svg.setAttribute("viewBox", "0 0 100 100");
 
         // Wrapper Position & Rotation
-        const x = Math.random() * 100;
-        const y = Math.random() * 100;
+        const posX = Math.random() * 100;
+        const posY = Math.random() * 100;
         const rot = Math.random() * 360;
         wrapper.style.position = "absolute";
-        wrapper.style.left = `${x}%`;
-        wrapper.style.top = `${y}%`;
+        wrapper.style.left = `${posX}%`;
+        wrapper.style.top = `${posY}%`;
         wrapper.style.transform = `translate(-50%, -50%) rotate(${rot}deg)`;
 
-        // Polygonfarbe
+        // Polygon color
         polygon.style.fill = colors[Math.floor(Math.random() * colors.length)];
 
-        // Punkte für Polygon
+        // Points ffor polygon
         let points = "0,0";
         for (let pointCount = 0; pointCount < 2 + Math.random() * 2; pointCount++) { 
             points += ` ${Math.random() * 100},${Math.random() * 100}`;

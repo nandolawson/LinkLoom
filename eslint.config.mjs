55b91896390e86d3bd156c907f8d5d02ddd86f07
Nando Lawson <37831410+nandolawson@users.noreply.github.com>
2025-10-18 15:59:47 +0000

    Declare themes.js as module

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,9 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 
-
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {files: ["**/*.js"], languageOptions: {sourceType: "script"}},
+  {files: ["scripts/theme.js"], languageOptions: {sourceType: "module"}},
   {languageOptions: { globals: globals.browser }},
   pluginJs.configs.all,
   {rules: {
